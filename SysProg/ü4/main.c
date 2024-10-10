d32ee2b51e7f8410bf139936718dd63fc0b0e887
RandomCad <mmont4850@gmail.com>
2024-10-10 14:46:56 +0200

    communication over pipes copyed from https://www.tutorialspoint.com/named-pipe-or-fifo-with-example-c-program

--- SysProg/ü4/main.c
+++ SysProg/ü4/main.c
@@ -1,30 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <stdlib.h>
 #include <unistd.h>
-void activeWait(const int a){
-  for(int i=0;i<a;i++){
-    for(int j=0;j<a;j++){
-      --j;++j;
-    }
-  }
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define FIFO_PATH "/tmp/myfifo" // Path to the named pipe
+
+void writer_process() {
+   int fd;
+   char message[] = "Hello, named pipe!";
+
+   // Open the named pipe for writing
+   fd = open(FIFO_PATH, O_WRONLY);
+   if (fd == -1) {
+      perror("open");
+      exit(EXIT_FAILURE);
+   }
+
+   // Write the message to the named pipe
+   write(fd, message, strlen(message) + 1);
+   printf("Message sent: %s", message);
+
+   // Close the named pipe
+   close(fd);
 }
 
-int main(int argc, const char * argv[]){
-  __pid_t pid = getpid();
-  fork();
-  if(pid == getpid()) printf("original\n");
-  else printf("new\n");
-  return 0;
-  srand(time(NULL));
-  int ran=rand();
-  for(int i=10;i<ran;i++){
-    activeWait(0xffffffff^0x10000000);
-  }
-  
-  printf("Hi i'm process: %d\n", getpid());
-  for(int i=10;i<(0xffffffff^0x10000000)-ran;i++){
-    activeWait(0xffffffff^0x10000000);
-  }
+void reader_process() {
+   int fd;
+   char buffer[100];
+
+   // Open the named pipe for reading
+   fd = open(FIFO_PATH, O_RDONLY);
+   if (fd == -1) {
+      perror("open");
+      exit(EXIT_FAILURE);
+   }
+
+   // Read the message from the named pipe
+   read(fd, buffer, sizeof(buffer));
+   printf("Message received: %s", buffer);
+
+   // Close the named pipe
+   close(fd);
+}
+
+int main() {
+   pid_t pid;
+
+   // Create the named pipe
+   mkfifo(FIFO_PATH, 0666);
+
+   // Fork a child process
+   pid = fork();
+
+   if (pid == -1) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+   } else if (pid == 0) {
+      // Child process (writer)
+      writer_process();
+   } else {
+      // Parent process (reader)
+      reader_process();
+
+      // Wait for the child process to finish
+      wait(NULL);
+
+      // Remove the named pipe
+      unlink(FIFO_PATH);
+   }
+   return 0;
 }
